e00fc027b914ade362327e266c11823a381338fc
Mindy Allen <mrsmallen07@gmail.com>
2019-10-23 17:14:55 -0400

    styling the signup and sign in box

--- mud-adventure/src/registration/RegistrationStyles.js
+++ mud-adventure/src/registration/RegistrationStyles.js
@@ -2,16 +2,33 @@ import { makeStyles } from "@material-ui/core";
 
 const RegistrationStyles = makeStyles(theme => ({
   container: {
+    backgroundColor: "#258f6c",
     display: "flex",
-    border: "1px solid red"
+   flexDirection: "column",
+   alignItems: "center",
+   width: "400px",
+   height: "340",
+  borderRadius: "12px",
+  boxShadow: "2px 2px 15px 3px rgba(0,0,0,0.5);",
+  margin: "0 auto 0 auto",
+  padding: "0px 0px 70px 0px",
+  border:  "#2980b9 4px solid"
   },
+
   header: {
-    fontSize: "3rem"
+    fontSize: "5rem",
+    
+   
   },
+
+
   form: {
     display: "flex",
-    flexDirection: "column"
+    flexDirection: "column",
+    
   },
+
+ 
   passwordError: {
     color: "red"
   },
@@ -21,6 +38,7 @@ const RegistrationStyles = makeStyles(theme => ({
   toAccount: {
     color: "blue",
     fontSize: "1.3rem"
+    
   }
 }));
 
