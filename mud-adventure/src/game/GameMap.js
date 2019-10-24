33563466dd55920dca35fb2af6c8ce98108572ff
christiansallen <christiansallen@gmail.com>
2019-10-24 13:38:52 -0600

    added arros to show directions

--- mud-adventure/src/game/GameMap.js
+++ mud-adventure/src/game/GameMap.js
@@ -8,11 +8,25 @@ import { faSquare as regSquare } from "@fortawesome/free-regular-svg-icons";
 import {
   faSquare,
   faCoffee,
-  faSquareFull
+  faSquareFull,
+  faArrowUp,
+  faArrowRight,
+  faArrowDown,
+  faArrowLeft
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-library.add(fab, faSquare, faCoffee, faSquareFull, regSquare);
+library.add(
+  fab,
+  faSquare,
+  faCoffee,
+  faSquareFull,
+  regSquare,
+  faArrowUp,
+  faArrowRight,
+  faArrowDown,
+  faArrowLeft
+);
 
 const GameMap = ({ currentRoom, current_coordinates }) => {
   const classes = GameStyles();
@@ -56,18 +70,50 @@ const GameMap = ({ currentRoom, current_coordinates }) => {
     for (let y = minY; y <= maxY; y++) {
       let rowMap = [];
       for (let x = minX; x <= maxX; x++) {
-        roomList.find(el => el.x === x && el.y === y)
-          ? rowMap.push(
-              <div className={classes.room} key={[x, y]}>
-                {x === current_coordinates[0] &&
-                y === current_coordinates[1] ? (
-                  <FontAwesomeIcon icon={faSquare} />
-                ) : (
-                  <FontAwesomeIcon icon={regSquare} />
-                )}
-              </div>
-            )
-          : rowMap.push(<div></div>);
+        let north = 0;
+        let south = 0;
+        let east = 0;
+        let west = 0;
+        if (roomList.find(el => el.x === x && el.y === y)) {
+          let room = roomList.find(el => el.x === x && el.y === y);
+          north = room.n_to;
+          south = room.s_to;
+          east = room.e_to;
+          west = room.w_to;
+          rowMap.push(
+            <div className={classes.room} key={[x, y]}>
+              {x === current_coordinates[0] && y === current_coordinates[1] ? (
+                <div className={classes.allArrows}>
+                  <div>
+                    {north > 0 ? <FontAwesomeIcon icon={faArrowUp} /> : " "}
+                  </div>
+                  <div>
+                    {west > 0 ? <FontAwesomeIcon icon={faArrowLeft} /> : " "}
+                    <FontAwesomeIcon icon={faSquare} />{" "}
+                    {east > 0 ? <FontAwesomeIcon icon={faArrowRight} /> : " "}
+                  </div>
+                  <div>
+                    {south > 0 ? <FontAwesomeIcon icon={faArrowDown} /> : " "}
+                  </div>
+                </div>
+              ) : (
+                <div className={classes.allArrows}>
+                  <div>
+                    {north > 0 ? <FontAwesomeIcon icon={faArrowUp} /> : " "}
+                  </div>
+                  <div>
+                    {west > 0 ? <FontAwesomeIcon icon={faArrowLeft} /> : " "}
+                    <FontAwesomeIcon icon={regSquare} />{" "}
+                    {east > 0 ? <FontAwesomeIcon icon={faArrowRight} /> : " "}
+                  </div>
+                  <div>
+                    {south > 0 ? <FontAwesomeIcon icon={faArrowDown} /> : " "}
+                  </div>
+                </div>
+              )}
+            </div>
+          );
+        } else rowMap.push(<div></div>);
       }
       map.push(<div className={classes.row}>{rowMap.map(row => row)}</div>);
     }
